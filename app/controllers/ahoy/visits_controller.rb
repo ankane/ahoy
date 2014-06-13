4a9d909a25a0dd8ab725dbf212f990a831ef2e34
Andrew Kane <acekane1@gmail.com>
2014-06-13 08:11:53 -0700

    Fixed regression with bot tracking - closes #21

--- app/controllers/ahoy/visits_controller.rb
+++ app/controllers/ahoy/visits_controller.rb
@@ -2,31 +2,7 @@ module Ahoy
   class VisitsController < BaseController
 
     def create
-      visit_token = params[:visit_token] || Ahoy.generate_id
-      visitor_token = params[:visitor_token] || Ahoy.generate_id
-
-      # TODO move to subscriber
-      visit =
-        Ahoy.visit_model.new do |v|
-          v.visit_token = visit_token
-          v.visitor_token = visitor_token
-          v.ip = request.remote_ip if v.respond_to?(:ip=)
-          v.user_agent = request.user_agent if v.respond_to?(:user_agent=)
-          v.referrer = params[:referrer] if v.respond_to?(:referrer=)
-          v.landing_page = params[:landing_page] if v.respond_to?(:landing_page=)
-          v.user = Ahoy.fetch_user(self) if v.respond_to?(:user=)
-          v.platform = params[:platform] if v.respond_to?(:platform=)
-          v.app_version = params[:app_version] if v.respond_to?(:app_version=)
-          v.os_version = params[:os_version] if v.respond_to?(:os_version=)
-        end
-
-      begin
-        visit.save!
-      rescue ActiveRecord::RecordNotUnique
-        # do nothing
-      end
-
-      render json: {visit_token: visit_token, visitor_token: visitor_token}
+      render json: ahoy.track_visit
     end
 
   end
--- lib/ahoy/tracker.rb
+++ lib/ahoy/tracker.rb
@@ -7,7 +7,7 @@ module Ahoy
     end
 
     def track(name, properties = {}, options = {})
-      if (Ahoy.track_bots or !bot?) and !exclude?
+      if track?
         # publish to each subscriber
         options = options.dup
         if @controller
@@ -39,7 +39,42 @@ module Ahoy
       true
     end
 
+    # not a public API - do not use
+    def track_visit
+      visit_token = params[:visit_token] || Ahoy.generate_id
+      visitor_token = params[:visitor_token] || Ahoy.generate_id
+
+      if track?
+        # TODO move to subscriber
+        visit =
+          Ahoy.visit_model.new do |v|
+            v.visit_token = visit_token
+            v.visitor_token = visitor_token
+            v.ip = request.remote_ip if v.respond_to?(:ip=)
+            v.user_agent = request.user_agent if v.respond_to?(:user_agent=)
+            v.referrer = params[:referrer] if v.respond_to?(:referrer=)
+            v.landing_page = params[:landing_page] if v.respond_to?(:landing_page=)
+            v.user = Ahoy.fetch_user(self) if v.respond_to?(:user=)
+            v.platform = params[:platform] if v.respond_to?(:platform=)
+            v.app_version = params[:app_version] if v.respond_to?(:app_version=)
+            v.os_version = params[:os_version] if v.respond_to?(:os_version=)
+          end
+
+        begin
+          visit.save!
+        rescue ActiveRecord::RecordNotUnique
+          # do nothing
+        end
+      end
+
+      {visit_token: visit_token, visitor_token: visitor_token}
+    end
+
     protected
+
+    def track?
+      (Ahoy.track_bots || !bot?) && !exclude?
+    end
 
     def bot?
       @bot ||= Browser.new(ua: @request.user_agent).bot?
@@ -55,6 +90,14 @@ module Ahoy
       else
         false
       end
+    end
+
+    def params
+      @controller.params
+    end
+
+    def request
+      @request
     end
 
   end
