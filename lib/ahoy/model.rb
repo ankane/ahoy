7951e8f56f4d0deb76c00a60f86cc2ed17cbd656
Andrew Kane <acekane1@gmail.com>
2014-06-15 02:29:06 -0700

    Removed ahoy_visit method

--- lib/ahoy/model.rb
+++ lib/ahoy/model.rb
@@ -1,19 +1,6 @@
 module Ahoy
   module Model
 
-    def ahoy_visit
-      class_eval do
-        belongs_to :user, polymorphic: true
-
-        def landing_params
-          @landing_params ||= begin
-            ActiveSupport::HashWithIndifferentAccess.new(Extractors::UtmParameterExtractor.new(landing_page).landing_params)
-          end
-        end
-
-      end
-    end
-
     def visitable(name = nil, options = {})
       if name.is_a?(Hash)
         name = nil
