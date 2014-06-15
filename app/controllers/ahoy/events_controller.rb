da0df80f86b5df739399fc509ebe3ea117acef46
Andrew Kane <acekane1@gmail.com>
2014-06-15 01:21:31 -0700

    Handle ISO 8601

--- app/controllers/ahoy/events_controller.rb
+++ app/controllers/ahoy/events_controller.rb
@@ -4,9 +4,14 @@ module Ahoy
     def create
       events = params[:name] ? [params] : ActiveSupport::JSON.decode(request.body.read)
       events.each do |event|
+        time = Time.zone.parse(event["time"]) rescue nil
+
+        # timestamp is deprecated
+        time ||= Time.zone.at(event["time"].to_f) rescue nil
+
         options = {
           id: event["id"],
-          time: (Time.zone.at(event["time"].to_f) rescue nil),
+          time: time,
           trusted: false
         }
         ahoy.track event["name"], event["properties"], options
