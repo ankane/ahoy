a244e2017114f6a707f198ecf8410a294ea19bbd
James Riley <jriley@betaforce.com>
2014-06-26 22:57:52 -0400

    Fix attempt to call #visit on NilClass

--- lib/ahoy/model.rb
+++ lib/ahoy/model.rb
@@ -13,7 +13,7 @@ module Ahoy
       end
       class_eval %Q{
         def set_visit
-          self.#{name} ||= RequestStore.store[:ahoy].visit
+          self.#{name} ||= RequestStore.store[:ahoy].try(:visit)
         end
       }
     end
