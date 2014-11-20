d39d41cd5fa4f80da016f437496206ad972605d7
Edwin Wills <edwin@rentify.com>
2014-11-20 21:18:56 +0000

    Allow controller filters specified to be run
    This means that if any project needs to implement Ahoy and run an
    initialiser before Ahoy tracking kicks in, it can be run in a specified
    controller filter.

--- app/controllers/ahoy/base_controller.rb
+++ app/controllers/ahoy/base_controller.rb
@@ -1,10 +1,20 @@
 module Ahoy
   class BaseController < ApplicationController
     # skip all filters
-    skip_filter *_process_action_callbacks.map(&:filter)
+    skip_filter :skip_filters
 
     def ahoy
       @ahoy ||= Ahoy::Tracker.new(controller: self, api: true)
+    end
+
+    private
+
+    def filters_to_allow
+      []
+    end
+
+    def skip_filters
+      BaseController._process_action_callbacks.map(&:filter).reject{ |filter| filters_to_allow.include?(filter) }
     end
 
   end
