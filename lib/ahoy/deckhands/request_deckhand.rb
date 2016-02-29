8e44b99ddbb5cbaf96960672d4e2929caf34a085
Juanjo Bazán <jjbazan@gmail.com>
2016-02-29 13:07:42 +0100

    Memoized request params

--- lib/ahoy/deckhands/request_deckhand.rb
+++ lib/ahoy/deckhands/request_deckhand.rb
@@ -17,31 +17,37 @@ module Ahoy
       end
 
       def referrer
-        @options[:api] ? request.params["referrer"] : request.referer
+        @options[:api] ? params["referrer"] : request.referer
       end
 
       def landing_page
-        @options[:api] ? request.params["landing_page"] : request.original_url
+        @options[:api] ? params["landing_page"] : request.original_url
       end
 
       def platform
-        request.params["platform"]
+        params["platform"]
       end
 
       def app_version
-        request.params["app_version"]
+        params["app_version"]
       end
 
       def os_version
-        request.params["os_version"]
+        params["os_version"]
       end
 
       def screen_height
-        request.params["screen_height"]
+        params["screen_height"]
       end
 
       def screen_width
-        request.params["screen_width"]
+        params["screen_width"]
+      end
+
+      private
+
+      def params
+        @params ||= request.params
       end
     end
   end
