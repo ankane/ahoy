89c2827a7aad73be97dffeaa94d4c6d102763d7f
Koen Punt <koen@koenpunt.nl>
2016-06-01 18:21:53 +0200

    skip_action is undefined

--- app/controllers/ahoy/base_controller.rb
+++ app/controllers/ahoy/base_controller.rb
@@ -2,8 +2,11 @@ module Ahoy
   class BaseController < ApplicationController
     # skip all filters except for authlogic
     filters = _process_action_callbacks.map(&:filter) - [:load_authlogic]
-    if respond_to?(:skip_action)
-      skip_action *filters
+
+    if respond_to?(:skip_before_action)
+      skip_before_action *filters
+      skip_after_action *filters
+      skip_around_action *filters
       before_action :verify_request_size
     else
       skip_filter *filters
