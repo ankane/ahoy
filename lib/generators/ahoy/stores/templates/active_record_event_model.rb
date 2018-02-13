6e995e915687d7b66518e9378b485848a67d5aa2
Pavan Katepalli <pavankatepalli@Pavans-MBP-2.home>
2018-02-13 13:30:01 -0800

    add optional: true for visit to event assoc

--- lib/generators/ahoy/stores/templates/active_record_event_model.rb
+++ lib/generators/ahoy/stores/templates/active_record_event_model.rb
@@ -4,7 +4,7 @@ module Ahoy
 
     self.table_name = "ahoy_events"
 
-    belongs_to :visit
+    belongs_to :visit<%= Rails::VERSION::MAJOR >= 5 ? ", optional: true" : nil %><% unless %w(postgresql postgresql-jsonb).include?(@database) %>
     belongs_to :user<%= Rails::VERSION::MAJOR >= 5 ? ", optional: true" : nil %><% unless %w(postgresql postgresql-jsonb).include?(@database) %>
 
     serialize :properties, JSON<% end %>
