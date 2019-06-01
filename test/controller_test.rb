5fe00d965c88c27acbac32c0e9e14fc41b342f21
Andrew Kane <andrew@chartkick.com>
2019-06-01 16:48:05 -0700

    Test cleanup

--- test/controller_test.rb
+++ test/controller_test.rb
@@ -1,22 +1,27 @@
 require_relative "test_helper"
 
-class UsersControllerTest < ActionDispatch::IntegrationTest
-  def test_ensure_token_removes_invalid_utf8_bytes_from_visit_token_cookie
+class ControllerTest < ActionDispatch::IntegrationTest
+  def test_works
+    get products_url
+    assert :success
+  end
+
+  def test_bad_visit_cookie
     make_request(cookies: {"ahoy_visit" => "badtoken\255"})
     assert_equal ahoy.visit_token, "badtoken"
   end
 
-  def test_ensure_token_removes_invalid_utf8_bytes_from_visitor_token_cookie
+  def test_bad_visitor_cookie
     make_request(cookies: {"ahoy_visitor" => "badtoken\255"})
     assert_equal ahoy.visitor_token, "badtoken"
   end
 
-  def test_ensure_token_removes_invalid_utf8_bytes_from_visit_token_header
+  def test_bad_visit_header
     make_request(headers: {"Ahoy-Visit" => "badtoken\255"})
     assert_equal ahoy.visit_token, "badtoken"
   end
 
-  def test_ensure_token_removes_invalid_utf8_bytes_from_visitor_token_header
+  def test_bad_visitor_header
     make_request(headers: {"Ahoy-Visitor" => "badtoken\255"})
     assert_equal ahoy.visitor_token, "badtoken"
   end
