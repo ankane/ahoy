724c84abb1565aac8b9395221026604cf5d6d3bc
Andrew Kane <andrew@chartkick.com>
2019-06-11 21:22:32 -0700

    Added timeout [skip ci]

--- test/support/mongoid.rb
+++ test/support/mongoid.rb
@@ -2,5 +2,5 @@ Mongoid.logger.level = Logger::WARN
 Mongo::Logger.logger.level = Logger::WARN
 
 Mongoid.configure do |config|
-  config.connect_to("ahoy_test")
+  config.connect_to("ahoy_test", server_selection_timeout: 1)
 end
