1682a42e7cbd03c4580c272e1ad5b4460dc72cb8
Andrew Kane <andrew@chartkick.com>
2019-06-11 21:23:04 -0700

    Fixed deprecation warning [skip ci]

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,7 +8,7 @@ require "mongoid"
 
 Combustion.path = "test/internal"
 Combustion.initialize! :all do
-  if config.active_record.sqlite3.respond_to?(:represent_boolean_as_integer)
+  if ActiveRecord::VERSION::MAJOR < 6 && config.active_record.sqlite3.respond_to?(:represent_boolean_as_integer)
     config.active_record.sqlite3.represent_boolean_as_integer = true
   end
 
