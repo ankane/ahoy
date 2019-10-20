cc6f59a02c45827c9011feb21392f23b70d69d32
Andrew Kane <andrew@chartkick.com>
2019-10-20 13:45:35 -0700

    Better test fix

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,7 +7,7 @@ require "active_record"
 require "mongoid"
 
 Combustion.path = "test/internal"
-Combustion.initialize! :all do
+Combustion.initialize! :active_record, :action_controller, :active_job do
   if ActiveRecord::VERSION::MAJOR < 6 && config.active_record.sqlite3.respond_to?(:represent_boolean_as_integer)
     config.active_record.sqlite3.represent_boolean_as_integer = true
   end
