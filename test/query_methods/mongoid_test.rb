c884e933397ec922c1953553f580fbe72a1b56f4
Andrew Kane <andrew@chartkick.com>
2020-03-18 17:32:20 -0700

    Test Mongoid with Rails 6 again

--- test/query_methods/mongoid_test.rb
+++ test/query_methods/mongoid_test.rb
@@ -10,17 +10,6 @@ end
 class MongoidTest < Minitest::Test
   include QueryMethodsTest
 
-  def setup
-    if Rails::VERSION::MAJOR == 6
-      @@once ||= begin
-        warn "Cannot test Rails 6 + Mongoid due to segmentation fault"
-        true
-      end
-      skip
-    end
-    super
-  end
-
   def model
     MongoidEvent
   end
