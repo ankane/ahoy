b5aa25f10c62d0707e35d5992b0b3cb31290226e
Andrew Kane <andrew@chartkick.com>
2020-07-25 19:33:39 -0700

    Improved API tests

--- test/api_test.rb
+++ test/api_test.rb
@@ -8,18 +8,51 @@ class ApiTest < ActionDispatch::IntegrationTest
     Ahoy::Event.delete_all
   end
 
-  def test_works
+  def test_visit
     visit_token = random_token
     visitor_token = random_token
 
     post ahoy_engine.visits_url, params: {visit_token: visit_token, visitor_token: visitor_token}
     assert :success
 
+    body = JSON.parse(response.body)
+    expected_body = {
+      "visit_token" => visit_token,
+      "visitor_token" => visitor_token,
+      "visit_id" => visit_token,
+      "visitor_id" => visitor_token
+    }
+    assert_equal expected_body, body
+
     assert_equal 1, Ahoy::Visit.count
 
     visit = Ahoy::Visit.last
     assert_equal visit_token, visit.visit_token
     assert_equal visitor_token, visit.visitor_token
+  end
+
+  def test_event
+    skip
+
+    visit = Ahoy::Visit.create!(visit_token: random_token, visitor_token: random_token)
+
+    name = "Test"
+    event_params = {
+      id: random_token,
+      name: name,
+      properties: {},
+      time: Time.current.iso8601,
+      visit_token: visit.visit_token,
+      visitor_token: visit.visitor_token
+    }
+    post ahoy_engine.events_url, params: event_params
+    assert :success
+
+    assert_equal 1, Ahoy::Event.count
+
+    event = Ahoy::Event.last
+    assert_equal visit, event.visit
+    assert_equal name, event.name
   end
 
   def random_token
