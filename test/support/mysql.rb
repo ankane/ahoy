310abfd93922864f2f6fc608fc0c70371880c8f3
Andrew Kane <andrew@ankane.org>
2021-07-29 11:26:46 -0700

    Revert "Use ActiveRecord::Schema.define for test setup [skip ci]"
    This reverts commit d8f869fdc0839b4a6126d839a749a33df925f6ed.

--- test/support/mysql.rb
+++ test/support/mysql.rb
@@ -1,13 +1,11 @@
 ActiveRecord::Base.establish_connection adapter: "mysql2", database: "ahoy_test"
 
-ActiveRecord::Schema.define do
-  create_table :mysql_text_events, force: true do |t|
-    t.text :properties
-  end
+ActiveRecord::Migration.create_table :mysql_text_events, force: true do |t|
+  t.text :properties
+end
 
-  create_table :mysql_json_events, force: true do |t|
-    t.json :properties
-  end
+ActiveRecord::Migration.create_table :mysql_json_events, force: true do |t|
+  t.json :properties
 end
 
 class MysqlBase < ActiveRecord::Base
--- test/support/postgresql.rb
+++ test/support/postgresql.rb
@@ -1,24 +1,22 @@
 ActiveRecord::Base.establish_connection adapter: "postgresql", database: "ahoy_test"
 
-ActiveRecord::Schema.define do
-  enable_extension "hstore"
+ActiveRecord::Migration.enable_extension "hstore"
 
-  create_table :postgresql_hstore_events, force: true do |t|
-    t.hstore :properties
-  end
+ActiveRecord::Migration.create_table :postgresql_hstore_events, force: true do |t|
+  t.hstore :properties
+end
 
-  create_table :postgresql_json_events, force: true do |t|
-    t.json :properties
-  end
+ActiveRecord::Migration.create_table :postgresql_json_events, force: true do |t|
+  t.json :properties
+end
 
-  create_table :postgresql_jsonb_events, force: true do |t|
-    t.jsonb :properties
-    t.index :properties, using: :gin
-  end
+ActiveRecord::Migration.create_table :postgresql_jsonb_events, force: true do |t|
+  t.jsonb :properties
+  t.index :properties, using: :gin
+end
 
-  create_table :postgresql_text_events, force: true do |t|
-    t.text :properties
-  end
+ActiveRecord::Migration.create_table :postgresql_text_events, force: true do |t|
+  t.text :properties
 end
 
 class PostgresqlBase < ActiveRecord::Base
