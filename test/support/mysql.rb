1cfff6ae3cd96af87574393bf63f5a294e33dd46
Andrew Kane <andrew@ankane.org>
2021-08-14 13:04:10 -0700

    Use ActiveRecord::Schema.define for test setup

--- test/support/mysql.rb
+++ test/support/mysql.rb
@@ -1,13 +1,13 @@
 ActiveRecord::Base.establish_connection adapter: "mysql2", database: "ahoy_test"
 
-# TODO use ActiveRecord::Schema.define when Active Record 5.0 is no longer supported
-# https://github.com/rails/rails/issues/28730
-ActiveRecord::Migration.create_table :mysql_text_events, force: true do |t|
-  t.text :properties
-end
+ActiveRecord::Schema.define do
+  create_table :mysql_text_events, force: true do |t|
+    t.text :properties
+  end
 
-ActiveRecord::Migration.create_table :mysql_json_events, force: true do |t|
-  t.json :properties
+  create_table :mysql_json_events, force: true do |t|
+    t.json :properties
+  end
 end
 
 class MysqlBase < ActiveRecord::Base
