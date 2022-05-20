262381d47cdd47b34ccf2e17d83e72d08145c123
Andrew Kane <andrew@ankane.org>
2022-05-20 02:44:14 -0700

    Added more generator tests

---
+++ test/base_generator_test.rb
@@ -0,0 +1,14 @@
+require_relative "test_helper"
+
+require "generators/ahoy/base_generator"
+
+class BaseGeneratorTest < Rails::Generators::TestCase
+  tests Ahoy::Generators::BaseGenerator
+  destination File.expand_path("../tmp", __dir__)
+  setup :prepare_destination
+
+  def test_works
+    run_generator
+    assert_file "config/initializers/ahoy.rb"
+  end
+end
--- test/mongoid_generator_test.rb
+++ test/mongoid_generator_test.rb
@@ -1,15 +1,13 @@
 require_relative "test_helper"
 
-require "generators/ahoy/activerecord_generator"
+require "generators/ahoy/mongoid_generator"
 
-class ActiverecordGeneratorTest < Rails::Generators::TestCase
-  tests Ahoy::Generators::ActiverecordGenerator
+class MongoidGeneratorTest < Rails::Generators::TestCase
+  tests Ahoy::Generators::MongoidGenerator
   destination File.expand_path("../tmp", __dir__)
   setup :prepare_destination
 
   def test_works
-    skip if ENV["ADAPTER"] == "mongoid"
-
     run_generator
     assert_file "config/initializers/ahoy.rb"
     assert_file "app/models/ahoy/visit.rb"
