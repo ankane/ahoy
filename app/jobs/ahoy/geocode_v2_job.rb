f8804c87072b4d48ffa9bc836583123c71f58dec
ts-3156 <ts_3156@yahoo.co.jp>
2023-04-15 05:13:04 +0900

    Fix "uninitialized constant Ahoy::ActiveJob"

--- app/jobs/ahoy/geocode_v2_job.rb
+++ app/jobs/ahoy/geocode_v2_job.rb
@@ -1,31 +1,36 @@
 module Ahoy
-  class GeocodeV2Job < ActiveJob::Base
-    queue_as { Ahoy.job_queue }
+  if defined?(ActiveJob) && defined?(ActiveJob::Base)
+    class GeocodeV2Job < ActiveJob::Base
+      queue_as { Ahoy.job_queue }
 
-    def perform(visit_token, ip)
-      location =
-        begin
-          Geocoder.search(ip).first
-        rescue NameError
-          raise "Add the geocoder gem to your Gemfile to use geocoding"
-        rescue => e
-          Ahoy.log "Geocode error: #{e.class.name}: #{e.message}"
-          nil
+      def perform(visit_token, ip)
+        location =
+            begin
+              Geocoder.search(ip).first
+            rescue NameError
+              raise "Add the geocoder gem to your Gemfile to use geocoding"
+            rescue => e
+              Ahoy.log "Geocode error: #{e.class.name}: #{e.message}"
+              nil
+            end
+
+        if location && location.country.present?
+          data = {
+              country: location.country,
+              country_code: location.try(:country_code).presence,
+              region: location.try(:state).presence,
+              city: location.try(:city).presence,
+              postal_code: location.try(:postal_code).presence,
+              latitude: location.try(:latitude).presence,
+              longitude: location.try(:longitude).presence
+          }
+
+          Ahoy::Tracker.new(visit_token: visit_token).geocode(data)
         end
-
-      if location && location.country.present?
-        data = {
-          country: location.country,
-          country_code: location.try(:country_code).presence,
-          region: location.try(:state).presence,
-          city: location.try(:city).presence,
-          postal_code: location.try(:postal_code).presence,
-          latitude: location.try(:latitude).presence,
-          longitude: location.try(:longitude).presence
-        }
-
-        Ahoy::Tracker.new(visit_token: visit_token).geocode(data)
       end
+    end
+  else
+    class GeocodeV2Job
     end
   end
 end
