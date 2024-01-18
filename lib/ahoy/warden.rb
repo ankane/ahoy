3349d278a922f644f37cf66cd12c6d7da79cada1
Sean Doyle <sean.p.doyle24@gmail.com>
2024-01-18 13:14:10 -0500

    Call `ahoy.reset` during Warden `before_logout` hook
    In addition to the `after_set_user` hook, the `Warden::Manager` also
    supports a [before_logout][] hook.
    
    This commit integrates with that hook to call `ahoy.reset` so that
    subsequent visits will be anonymous, and subsequent authenticated
    sessions will create a new Visit.
    
    [before_logout]: https://www.rubydoc.info/gems/warden/1.2.6/Warden/Hooks#before_logout-instance_method

--- lib/ahoy/warden.rb
+++ lib/ahoy/warden.rb
@@ -3,3 +3,9 @@ Warden::Manager.after_set_user except: :fetch do |user, auth, _|
   ahoy = Ahoy::Tracker.new(request: request)
   ahoy.authenticate(user)
 end
+
+Warden::Manager.before_logout do |_, auth, _|
+  request = ActionDispatch::Request.new(auth.env)
+  ahoy = Ahoy::Tracker.new(request: request)
+  ahoy.reset
+end
